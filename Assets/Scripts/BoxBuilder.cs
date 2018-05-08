770805feca24424b9dc00b1d3c2a6abf79f66c32
TeiKiyohira <qingping1998@gmail.com>
2018-05-08 18:01:30 +0800

    TONGS TESTING ROOM COMPLETE

---
+++ Assets/Scripts/BoxBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBuilder : MonoBehaviour {
+
+    [Tooltip("This determines the size of your test area.")]
+    public Vector2 sizeOfBox;
+
+    [Tooltip("Offset the starting area of the Box, shifting it by this amount")]
+    public Vector2 offset;
+
+    public GameObject BoxPrefab;
+
+    private void Start()
+    {
+        GameObject TestBox = new GameObject();
+
+        for (int y = 0; y < sizeOfBox.y; y++)
+        {
+            for (int x = 0; x < sizeOfBox.x; x++)
+            {
+                if (x == 0 || y == 0 || x == sizeOfBox.x - 1 || y == sizeOfBox.y - 1)
+                {
+                    GameObject cell = Instantiate(BoxPrefab, new Vector3(x + offset.x, y + offset.y, 0), Quaternion.identity);
+                    cell.name = "BOX X:" + (x + offset.x).ToString() + "Y:" + (y + offset.y).ToString();
+                    cell.transform.SetParent(TestBox.transform);
+                }
+            }
+        }
+
+        TestBox.name = "Box Holder";
+    }
+}
