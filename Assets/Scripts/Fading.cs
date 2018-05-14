f41167ce2d46a0b0a29f35f1c34e65eb9884fbab
TeiKiyohira <qingping1998@gmail.com>
2018-05-14 16:43:32 +0800

    More cleanup, fading between scenes (adjustable fades)

--- Assets/Scripts/Fading.cs
+++ Assets/Scripts/Fading.cs
@@ -5,28 +5,58 @@ using System.Collections;
 
 public class Fading : MonoBehaviour
 {
+    public bool activeOnLoad = false;
+    public float fadeInTime = 1.5f;
+    public float fadeOutTime = 2.5f;
     public Image splashImage;
     public string loadLevel;
 
+    public void StartFade()
+    {
+        StartCoroutine(LoadScene());
+        //level loader uses the index if needed.
+    }
 
-    IEnumerator Start()
+    IEnumerator LoadScene()
     {
         splashImage.canvasRenderer.SetAlpha(0.0f);
 
         FadeIn();
-        yield return new WaitForSeconds(2.5f);
-        FadeOut();
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(fadeOutTime);
+        //FadeOut();
+        //yield return new WaitForSeconds(2.5f);
         SceneManager.LoadScene(loadLevel);
+    }
+
+    IEnumerator Start()
+    {
+        if(activeOnLoad)
+        {
+            splashImage.enabled = true;
+            splashImage.canvasRenderer.SetAlpha(0.0f);
+
+            FadeIn();
+            yield return new WaitForSeconds(fadeOutTime);
+            FadeOut();
+            yield return new WaitForSeconds(fadeOutTime);
+            SceneManager.LoadScene(loadLevel);
+        }
+        else
+        {
+            splashImage.enabled = true;
+            FadeOut();
+            yield return new WaitForSeconds(fadeOutTime);
+            splashImage.enabled = false;
+        }
     }
 
     void FadeIn()
     {
-        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+        splashImage.CrossFadeAlpha(1.0f, fadeInTime, false);
     }
 
     void FadeOut()
     {
-        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+        splashImage.CrossFadeAlpha(0.0f, fadeOutTime, false);
     }
 }
